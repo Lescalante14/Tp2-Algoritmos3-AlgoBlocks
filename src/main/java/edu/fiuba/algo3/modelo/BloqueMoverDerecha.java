65d292303789b582b468e870468330509fa81619
andrezszambrano <andreszambrano1605@gmail.com>
2021-02-02 20:19:46 -0300

    commit de prueba a develop

--- src/main/java/edu/fiuba/algo3/modelo/BloqueMoverDerecha.java
+++ src/main/java/edu/fiuba/algo3/modelo/BloqueMoverDerecha.java
@@ -1,10 +1,10 @@
 package edu.fiuba.algo3.modelo;
 
-public class BloqueMoverDerecha extends Bloque{
+public class   BloqueMoverDerecha extends Bloque{
 
     @Override
     public void ejecutar(Personaje unPersonaje) {
-        unPersonaje.moverADerecha();
+        unPersonaje.moverADerecha() ;
     }
 
 }
