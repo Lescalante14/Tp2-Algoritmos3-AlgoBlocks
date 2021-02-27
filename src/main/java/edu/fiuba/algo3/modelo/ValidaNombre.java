c36b81c53553c985eca4f916bcccaef76ccf54ac
SantiC8 <scuretti@fi.uba.ar>
2021-02-27 10:20:58 -0300

    Excepciones

--- src/main/java/edu/fiuba/algo3/modelo/ValidaNombre.java
+++ src/main/java/edu/fiuba/algo3/modelo/ValidaNombre.java
@@ -1,11 +1,12 @@
-package edu.fiuba.algo3.excepciones.*;
+package edu.fiuba.algo3.modelo;
+import edu.fiuba.algo3.excepciones.NombreVacioError;
 
 public class ValidaNombre{
 	
 	public static void nombreValido(String nombre){
 
 		if( (nombre == null) || (nombre.trim().length() > 0)){
-			return new NombreVacioError();			
+			return new NombreVacioError();
 		}else{
 				return true;
 		}
